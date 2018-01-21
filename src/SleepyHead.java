f7e19a40ea0ed201338f00274a79f51eae1421b8
Punky64 <league@WTS-iMac-11.attlocal.net>
2018-01-21 15:54:42 -0800

    Sleepy Head

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,28 +1,38 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /*
-         * Ask the user for these values using
-         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-         * JOptionPane.YES_NO_OPTION);
-         */
-    
+		boolean isWeekday, isVacation;
 
+		/*
+		 * Ask the user for these values using JOptionPane.showConfirmDialog(null,
+		 * "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		 */
+		int ans = JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
 
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
-         */
-    }
+		if (ans == 0) {
+			int ans2 = JOptionPane.showConfirmDialog(null, "are you on vacation", "", JOptionPane.YES_NO_OPTION);
+			if (ans2 == 0) {
+				JOptionPane.showMessageDialog(null, "you may sleep");
+			} else {
+				JOptionPane.showMessageDialog(null, "Dood wake up");
+
+			}
+		}
+
+		if (ans == 1) {
+			JOptionPane.showMessageDialog(null, "you may sleep");
+		}
+		/*
+		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!” If it is a weekday, and we are on vacation, print “sleep
+		 * in”.
+		 */
+	}
 }
